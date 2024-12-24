0dfbd44cd5617d0ee703575121fbbb88d6a4296a
Shweta0718 <shwetahiranwale0918@gmail.com>
2024-12-24 10:19:44 +0530

    Add files via upload

---
+++ areare.py
@@ -0,0 +1,9 @@
+#you can give default input to the variables
+width = 4
+height = 5
+
+# formula to calculate the Area
+Area = width * height
+
+# after calculating,
+print("Area of rectangle = "+str(Area))  
---
+++ si.py
@@ -0,0 +1,11 @@
+# for given principal amount, time and
+# rate of interest.
+
+
+def simple_interest(p,t,r):
+    si = (p * t * r)/100
+    
+    print('The Simple Interest is', si)
+    return si
+
+simple_interest(8, 9, 9)
